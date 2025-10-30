bc576b92953ad09bf4ab7a601d4a7aebd00023cb
Hamza <hamzashafique@thesoftaims.com>
2025-10-30 18:25:30 +0500

    ui ep-01

--- client/src/components/form/Navbar.tsx
+++ client/src/components/form/Navbar.tsx
@@ -1,21 +1,32 @@
-//client/src/components/form/Navbar.tsx
 /** @format */
 import React from "react";
 
 export default function Navbar() {
   return (
-    <nav className="w-full bg-[#070123] shadow-md border-b border-[#1f1b3a] pt-3 pb-3 pl-40 pr-40  top-0 left-0 z-100">
-      <div className=" mx-auto flex items-center justify-between  ">
-        <div className="flex items-center space-x-275">
+    <nav className="w-full bg-[#070123] shadow-md border-b border-[#1f1b3a] py-3 top-0 left-0 z-[100]">
+      <div
+        className="
+          mx-auto
+          grid 
+          grid-cols-2 
+          items-center
+        "
+      >
+        {/* Left Logo */}
+        <div className="flex justify-start pl-10 md:pl-40">
           <img
             src="/nav_pic.png"
-            alt="Logo"
-            className="md:h-16 h-full w-full object-contain "
+            alt="Logo Left"
+            className="h-10 md:h-16 w-auto object-contain"
           />
+        </div>
+
+        {/* Right Logo */}
+        <div className="flex justify-end pr-10 md:pr-40">
           <img
             src="/nav_pic2.png"
-            alt="Logo"
-            className="md:h-14 h-10 w-full object-contain"
+            alt="Logo Right"
+            className="h-10 md:h-16 w-auto object-contain"
           />
         </div>
       </div>
